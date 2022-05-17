d0c8adc31dd598c12633ff1bad836cc966a4a037
BERCUT38 <bazzz@list.ru>
2022-05-17 12:18:39 +0300

    workflow4

--- backend/sp14/apps/tags/admin.py
+++ backend/sp14/apps/tags/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+
 from .models import Tags
 
 
 @admin.register(Tags)
 class TagsAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
+
--- backend/sp14/apps/user/views.py
+++ backend/sp14/apps/user/views.py
@@ -1,11 +1,12 @@
+from apps.pagination import CustomPageNumberPaginator
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from rest_framework import generics, status, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from .models import Follow
 from .serializers import FollowSerializer, ShowFollowSerializer
-from apps.pagination import CustomPageNumberPaginator
 
 User = get_user_model()
 
