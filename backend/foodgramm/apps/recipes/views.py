9137c72a19f3d7e5e696214749d9546502fb0bfe
BERCUT38 <bazzz@list.ru>
2022-05-19 15:21:44 +0300

    new_wawe

--- backend/foodgramm/apps/recipes/views.py
+++ backend/foodgramm/apps/recipes/views.py
@@ -9,8 +9,8 @@ from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from apps.ingredients.models import Ingredient
 from apps.pagination import CustomPageNumberPaginator
+from apps.ingredients.models import Ingredient
 
 from .filters import RecipeFilter
 from .models import Favorite, Recipes, Ringredients, ShoppingList
