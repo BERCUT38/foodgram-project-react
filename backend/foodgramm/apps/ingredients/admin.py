89497345712fe0c0db5853937941acf45044074e
BERCUT38 <bazzz@list.ru>
2022-05-24 00:28:18 +0300

    finalv1

--- backend/foodgramm/apps/ingredients/admin.py
+++ backend/foodgramm/apps/ingredients/admin.py
@@ -6,5 +6,5 @@ from .models import Ingredient
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
-    search_fields = ('name', 'id',) 
-    list_filter = ('name',)    
+    search_fields = ('name', 'id',)
+    list_filter = ('name',)
